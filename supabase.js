3ec7b83e35bffab80f4b8a970927f5496d0d3bb4
tharushi330 <tharushiwarangana78@gmail.com>
2025-07-16 13:17:56 +0530

    connect supabase to app

---
+++ supabase.js
@@ -0,0 +1,7 @@
+// lib/supabase.js
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = 'https://hxlqgvacixgafzoyqysw.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh4bHFndmFjaXhnYWZ6b3lxeXN3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI2Mzk1MTcsImV4cCI6MjA2ODIxNTUxN30.rpg7OGUZJDVVTOEldXg-6SlBuKCzCgzsMTR5O3xuobs'
+
+export const supabase = createClient(supabaseUrl, supabaseKey)
