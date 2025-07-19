bfc8e86584e6f3c6dac576f1185f75ef7b4a3b36
tharushi330 <tharushiwarangana78@gmail.com>
2025-07-19 09:53:29 +0530

    update

---
+++ src/app/lib/stripe.ts
@@ -0,0 +1,10 @@
+// lib/stripe.ts
+
+import Stripe from 'stripe';
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+  apiVersion: '2025-06-30.basil' as any, 
+});
+
+
+export default stripe;
