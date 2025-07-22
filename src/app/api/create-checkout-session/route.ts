369688ab5e78854dc18ed860f07a987718210006
tharushi330 <tharushiwarangana78@gmail.com>
2025-07-22 18:39:07 +0530

    change url

--- src/app/api/create-checkout-session/route.ts
+++ src/app/api/create-checkout-session/route.ts
@@ -22,7 +22,7 @@ export async function POST(req: NextRequest) {
           quantity: 1,
         },
       ],
-      success_url: `https://blog-app-4god.vercel.app/payment-success`,
+      success_url: `http://localhost:3000/success`,
       cancel_url: `http://localhost:3000/cancel`,
     });
 
