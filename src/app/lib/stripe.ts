c787b6797b6628f11c4fec41eee3770df84f4914
tharushi330 <tharushiwarangana78@gmail.com>
2025-07-22 14:44:27 +0530

    change stripe.ts

--- src/app/lib/stripe.ts
+++ src/app/lib/stripe.ts
@@ -3,7 +3,7 @@
 import Stripe from 'stripe';
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2025-06-30.basil' as any, 
+  apiVersion: '2025-06-30.basil' as const, 
 });
 
 
